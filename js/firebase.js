517dfcd4618e54171eb52c27a550b8137bf2e1b3
Alberto Alvarado <albertoalvarado@Albertos-MacBook-Pro-2.local>
2024-05-06 21:39:39 -0700

    working version with carousel

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlj5vAoLTuIgB2NlBerfKxoKHnyWuSfR4",
+  authDomain: "users-99a99.firebaseapp.com",
+  projectId: "users-99a99",
+  storageBucket: "users-99a99.appspot.com",
+  messagingSenderId: "480861713387",
+  appId: "1:480861713387:web:d322c50306d303e32edfae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
