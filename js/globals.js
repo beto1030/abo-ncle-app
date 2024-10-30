e6d6aa7c62333ac2db2e49400caa23ff5faada1e
Alberto Alvarado <albertoalvarado@MacBookPro.attlocal.net>
2024-10-29 19:00:45 -0700

    organized my javascript code. made sectionTitles dynamic with generateSections.js creating what i had hardcoded, now creates sectionTitles based on my files in sections directory.

---
+++ js/globals.js
@@ -0,0 +1,16 @@
+// globals.js
+export let currentSlideIndex = 0;
+
+export function setCurrentSlideIndex(index) {
+    currentSlideIndex = index;
+}
+
+export let slides = [];
+
+
+// Define section titles
+export const sectionTitles = {
+    'anatomy': 'Anatomy',
+    'physiology': 'Physiology',
+    'pathology': 'Pathology'
+};
